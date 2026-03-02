762316fae40087644e3e59166520bd58566b7f3c
HarshGautam-code <149058537+HarshGautam-code@users.noreply.github.com>
2026-03-02 16:04:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 //your code here!
 
+const list = document.getElementById("infi-list");
+
+let count = 0;
+
+// function to add items
+function addItems(num) {
+  for (let i = 0; i < num; i++) {
+    count++;
+    const li = document.createElement("li");
+    li.textContent = "List Item " + count;
+    list.appendChild(li);
+  }
+}
+
+// add 10 items by default
+addItems(10);
+
+// infinite scroll logic
+list.addEventListener("scroll", function () {
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+    addItems(2);
+  }
+}); 
